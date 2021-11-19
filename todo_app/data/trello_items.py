9e175691239112d8b315e40f147e5c6bb2c932ae
Peter Robinson <peter.robinson@softwire.com>
2021-11-19 17:06:41 +0000

    Exercise-2: add trello api integration

---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,116 @@
+import os
+from requests import request, exceptions
+import itertools
+
+from todo_app.data.item import Item
+from todo_app.data.list import List
+
+trello_base_url = "https://api.trello.com" 
+api_key = os.environ.get('TRELLO_API_KEY')
+api_token = os.environ.get('TRELLO_TOKEN')
+board_id = os.environ.get('TRELLO_BOARD_ID')
+
+def trello_request(method, path, query_params = []):
+    headers = {
+        "Accept": "application/json"
+    }
+    url = f"{trello_base_url}{path}?key={api_key}&token={api_token}"
+
+    for query_param in query_params:
+        url += f"&{query_param}"
+
+    try:
+        response = request(
+            method,
+            url,
+            headers=headers
+        )
+        response.raise_for_status()
+    except exceptions.HTTPError as err:
+        print(err)
+    
+    return response.json()
+
+def get_items():
+    """
+    Fetches all saved items from trello.
+
+    Returns:
+        list: The list of saved items.
+    """
+    def parse_column(column):
+        return [Item.from_trello_card(card, column) for card in column["cards"]]
+    
+    response = trello_request("GET", f"/1/boards/{board_id}/lists", ["cards=open"])
+
+    return sorted(list(itertools.chain(*[parse_column(col) for col in response])), key=lambda item: item.id)
+
+def get_item(id):
+    """
+    Fetches the saved item with the specified ID.
+
+    Args:
+        id: The ID of the item.
+
+    Returns:
+        item: The saved item, or None if no items match the specified ID.
+    """
+    items = get_items()
+    return next((item for item in items if item['id'] == int(id)), None)
+
+def get_lists():
+    """
+    Gets all the lists for the trello board
+
+    Returns:
+        lists: a list of List objects
+    """
+    response = trello_request("GET", f"/1/boards/{board_id}/lists")
+    return [List.from_trello_list(trello_list) for trello_list in response]
+
+def get_list_id_from_name(name):
+    """
+    Gets the trello list id of a list with the given name, or None if it does not exist
+    
+    Args:
+        name: The name of the list.
+
+    Returns:
+        listId: the id of the list, or None if it does not exist 
+    """
+    trello_lists = get_lists()
+    return next((trello_list.id for trello_list in trello_lists if trello_list.name == name), None)
+
+def add_item(title):
+    """
+    Adds a new item with the specified title to the session.
+
+    Args:
+        title: The title of the item.
+
+    Returns:
+        item: The saved item.
+    """
+
+    todo_id = get_list_id_from_name("To Do")
+    trello_request("POST", f"/1/cards", [f"idList={todo_id}", f"name={title}"])
+
+def mark_item_as_complete(id):
+    """
+    Marks an existing item on the trello board as complete. If no existing item matches the ID of the specified item, nothing is marked as complete
+
+    Args:
+        id: The ID of the item to mark as complete
+    """
+
+    done_id = get_list_id_from_name("Done")
+    trello_request("PUT", f"/1/cards/{id}", [f"idList={done_id}"])
+
+def remove_item(id):
+    """
+    Removes an existing item from the board. If no existing item matches the ID of the specified item, no item is removed
+
+    Args:
+        id: The ID of the item to remove
+    """
+    trello_request("DELETE", f"/1/cards/{id}")
