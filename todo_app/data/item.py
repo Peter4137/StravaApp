ccc6f1814bdd9af0659896fcccdc4afdf6b5ebf7
Peter Robinson <peter.robinson@softwire.com>
2022-08-02 13:04:43 +0100

    Exercise-10: Move over to using mongo DB

--- todo_app/data/item.py
+++ todo_app/data/item.py
@@ -5,7 +5,15 @@ class Item:
         self.id = id
         self.name = name
         self.status = status
+
+    def __init__(self, item):
+        self.id = item["_id"]
+        self.name = item["name"]
+        self.status = item["status"]
         
     @classmethod
     def from_trello_card(cls, card, list):
         return cls(card['id'], card['name'], list['name']) 
+
+    def __repr__(self):
+        return str({"id": self.id, "name": self.name, "status": self.status})
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import itertools
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 from todo_app.data.item import Item
 from todo_app.data.list import List
@@ -9,29 +11,9 @@ from todo_app.data.status import Status
 class TrelloItems:
 
     def __init__(self) -> None:
-        self.trello_base_url = "https://api.trello.com" 
-        self.api_key = os.environ.get("TRELLO_API_KEY")
-        self.api_token = os.environ.get('TRELLO_TOKEN')
-        self.board_id = os.environ.get('TRELLO_BOARD_ID')
-        self.organization_id = os.environ.get('TRELLO_ORGANIZATION_ID')
-
-    def trello_request(self, method, path, query_params = {}):
-        headers = {
-            "Accept": "application/json"
-        }
-        query_params["key"] = self.api_key
-        query_params["token"] = self.api_token
-        url = f"{self.trello_base_url}{path}"
-
-        response = requests.request(
-            method,
-            url,
-            headers=headers,
-            params=query_params
-        )
-        response.raise_for_status()
-
-        return response.json()
+        self.client = MongoClient(os.environ.get('DATABASE_CONNECTION_STRING'))
+        self.db = self.client.todo_app_database
+        self.items = self.db.items
 
     def get_items(self):
         """
@@ -40,12 +22,9 @@ class TrelloItems:
         Returns:
             list: The list of saved items.
         """
-        def parse_column(column):
-            return [Item.from_trello_card(card, column) for card in column["cards"]]
-        
-        response = self.trello_request("GET", f"/1/boards/{self.board_id}/lists", {"cards": "open"})
-
-        return sorted(list(itertools.chain(*[parse_column(col) for col in response])), key=lambda item: item.id)
+        items = self.items.find()
+        parsed_items = [Item(item) for item in items]
+        return sorted(parsed_items, key=lambda item: item.id)
 
     def get_item(self, id):
         """
@@ -57,31 +36,8 @@ class TrelloItems:
         Returns:
             item: The saved item, or None if no items match the specified ID.
         """
-        items = self.get_items()
-        return next((item for item in items if item.id == id), None)
-
-    def get_lists(self):
-        """
-        Gets all the lists for the trello board
-
-        Returns:
-            lists: a list of List objects
-        """
-        response = self.trello_request("GET", f"/1/boards/{self.board_id}/lists")
-        return [List.from_trello_list(trello_list) for trello_list in response]
-
-    def get_list_id_from_name(self, status_name: Status):
-        """
-        Gets the trello list id of a list with the given name, or None if it does not exist
-        
-        Args:
-            name: The name of the list.
-
-        Returns:
-            listId: the id of the list, or None if it does not exist 
-        """
-        trello_lists = self.get_lists()
-        return next((trello_list.id for trello_list in trello_lists if trello_list.name ==status_name.value), None)
+        item = self.items.find_one({"_id": ObjectId(id)})
+        return Item(item)
 
     def add_item(self, title):
         """
@@ -93,9 +49,11 @@ class TrelloItems:
         Returns:
             item: The saved item.
         """
-
-        todo_id = self.get_list_id_from_name(Status.ToDo)
-        self.trello_request("POST", f"/1/cards", {"idList": todo_id, "name": title})
+        item = {
+            "name": title,
+            "status": Status.ToDo.value
+        }
+        self.items.insert_one(item)
 
     def progress_item(self, id):
         """
@@ -105,8 +63,7 @@ class TrelloItems:
             id: The ID of the item to progress 
         """
         next_status = Status.get_next(self.get_item(id).status)
-        status_id = self.get_list_id_from_name(next_status)
-        self.trello_request("PUT", f"/1/cards/{id}", {"idList": status_id})
+        self.items.update_one({"_id": ObjectId(id)}, {"$set": {"status": next_status.value}})
 
     def remove_item(self, id):
         """
@@ -115,19 +72,4 @@ class TrelloItems:
         Args:
             id: The ID of the item to remove
         """
-        self.trello_request("DELETE", f"/1/cards/{id}")
-
-
-    def create_board(self, name):
-        """
-        Creates a new board in trello
-        """
-        response = self.trello_request("POST", f"/1/boards", {"name": name, "idOrganization": self.organization_id})
-        return response["id"]
-
-    def delete_board(self, id):
-        """ 
-        Deletes the board with the given ID
-        """
-
-        self.trello_request("DELETE", f"/1/boards/{id}")
+        self.items.delete_one({"_id": ObjectId(id)})
